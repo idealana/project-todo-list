09234f129689728cae8e96a1a50d1f2d8ad735eb
lalala <uhuylalala@gmail.com>
2022-08-16 10:56:57 +0700

    -- project todo list, edit Resource add method new add list resource

--- app/Http/Resources/Resources.php
+++ app/Http/Resources/Resources.php
@@ -8,7 +8,11 @@ final class Resources
      * Instace of resources
      */
     private static $resources = [
+        'project' => \App\Http\Resources\ProjectResource::class,
         'project_user' => \App\Http\Resources\ProjectUserResource::class,
+        'project_user_lists' => \App\Http\Resources\ProjectUserListResource::class,
+        'user' => \App\Http\Resources\UserResource::class,
+        'user_todo_list' => \App\Http\Resources\ProjectTodoListResource::class,
     ];
 
     /**
@@ -18,5 +22,14 @@ final class Resources
     {
         $resource = self::$resources[$resourceName];
         return $resource::collection($collections);
+    }
+
+    /**
+     * Transform the collection into an array.
+     */
+    public static function new($resourceName, $collection)
+    {
+        $resource = self::$resources[$resourceName];
+        return new $resource($collection);
     }
 }
