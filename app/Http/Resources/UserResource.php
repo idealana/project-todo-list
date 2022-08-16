dc0ac1848d35bf81013399fbe967b5d481501001
lalala <uhuylalala@gmail.com>
2022-08-16 10:52:19 +0700

    -- project todo list, user resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $user = parent::toArray($request);
+        unset($user['email_verified_at']);
+        return $user;
+    }
+}
